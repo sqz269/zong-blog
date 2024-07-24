a7f36fcb24f68c19f5d1ed4cb6aade46423a01e5
sqz269 <yin2690855381@gmail.com>
2024-07-24 10:31:15 -0400

    Added support for LaTeX in MDX

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -79,6 +79,13 @@ module.exports = {
           {
             resolve: `gatsby-remark-prismjs`,
           },
+          {
+            resolve: `gatsby-remark-katex`,
+            options: {
+              // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
+              strict: `ignore`
+            }
+          }
         ],
         mdxOptions: {
           remarkPlugins: [
